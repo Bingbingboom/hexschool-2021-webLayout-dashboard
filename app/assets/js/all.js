f737024be8c3054790cc4944273251f76f798cd8
Bingbingboom <50125519+Bingbingboom@users.noreply.github.com>
2021-07-12 00:59:18 +0800

    fisrt finished

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,29 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+
+$(document).ready(function(){
+
+  // Assignment 對話欄
+  $('.js-reply').click(function(event){
+    event.preventDefault();
+    $('.replyBtn').toggleClass('d-none');
+    $('.assignmentPage').toggleClass('d-none');
+    $('.replyText').toggleClass('d-none');
+    $('.replyInput').toggleClass('d-none');
+  });
+
+  $('.js-replyCancel').click(function(event){
+    event.preventDefault();
+    $('.replyBtn').toggleClass('d-none');
+    $('.assignmentPage').toggleClass('d-none');
+    $('.replyText').toggleClass('d-none');
+    $('.replyInput').toggleClass('d-none');
+  });
+
+  // view modal 下拉按鈕
+  $('.js-dropDown').click(function(event){
+    $('.js-dropDown').toggleClass('dropDownClick');
+    $('.js-dropDownBtn').toggleClass('d-none');
+  });
 });
